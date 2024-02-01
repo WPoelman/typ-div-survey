25023e58a28a7c1a02274c35ed1e581e6b162cc0
Wessel Poelman <contact+github@wesselpoelman.nl>
2024-02-01 13:11:13 +0100

    improve readability of plots

--- constants.py
+++ constants.py
@@ -6,6 +6,23 @@ LONG_TAIL_CUTOFF = 7
 
 ALTAIR_SETTINGS = {"opacity": OPACITY, "color": COLOR}
 
+AXIS_SETTINGS = {"labelFontSize": 15, "labelFont": "serif"}
+
+LEGEND_SETTINGS = {
+    "orient": "none",
+    "labelFontSize": 14,
+    "titleFontSize": 14,
+    "labelFont": "serif",
+    "titleFont": "serif",
+    "direction": "horizontal",
+    "title": "Introduces Dataset",
+    "values": ["yes", "no"],
+    "titleAnchor": "middle",
+    "fillColor": "white",
+    "strokeColor": "black",
+    "padding": 4,
+}
+
 URIEL_UNKNOWN = {"hsi", "poma", "ypk", "nan"}
 
 GB_FEATURE_MISSING = {"no_cov", "?"}
